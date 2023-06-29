8b4b6891e4cac83e723bf4c2bd8aa939b93daba0
trevharri <trevor.harri94@gmail.com>
2023-06-29 12:17:21 +0200

    updated logging levels

--- tests/my_account/test_login_negative.py
+++ tests/my_account/test_login_negative.py
@@ -10,16 +10,18 @@ class TestLoginNegative:
     def test_login_non_existing_user(self):
         log = get_logger()
         my_account = MyAccountSignedOut(self.driver)
-
-        log.info("Going to my account page.")
-        my_account.go_to_my_account()
-        log.info("Entering username/password that aren't registered")
-        my_account.input_login_username("TestUser")
-        my_account.input_login_password("TestPassword1234*")
-        log.info("Click login button")
-        my_account.click_login_button()
-        log.info("Asserting error message appears.")
-        err_text = my_account.get_err_text()
+        user_name = "TestUser"
+        password = "TestPassword1234*"
         expected_err_text = 'Error: The username TestUser is not registered on this site. If you are unsure of your ' \
                             'username, try your email address instead.'
+
+        log.debug("Going to my account page.")
+        my_account.go_to_my_account()
+        log.info(f"Entering unregistered username/password ({user_name}/{password}) that aren't registered")
+        my_account.input_login_username(user_name)
+        my_account.input_login_password(password)
+        log.debug("Click login button")
+        my_account.click_login_button()
+        log.info(f"Asserting error message appears: {expected_err_text}")
+        err_text = my_account.get_err_text()
         assert err_text == expected_err_text
--- tests/my_account/test_register_new_user.py
+++ tests/my_account/test_register_new_user.py
@@ -15,13 +15,14 @@ class TestRegisterNewUser:
         my_account_signed_out = MyAccountSignedOut(self.driver)
         my_account_signed_in = MyAccountSignedIn(self.driver)
         random_credentials = generate_random_email_and_password()
+        password = "QwertyQwerty1234!@#$"
 
-        log.info("Going to my account page")
+        log.debug("Going to my account page")
         my_account_signed_out.go_to_my_account()
-        log.info(f'Generating/inputting email: {random_credentials["email"]} and password: QwertyQwerty1234!@#$ ')
+        log.info(f'Generating/inputting email: {random_credentials["email"]} and password: {password} ')
         my_account_signed_out.input_reg_email(random_credentials["email"])
-        my_account_signed_out.input_reg_password('QwertyQwerty1234!@#$')
-        log.info("Clicking register button")
+        my_account_signed_out.input_reg_password(password)
+        log.debug("Clicking register button")
         my_account_signed_out.click_reg_button()
         log.info("Asserting Log out button present on new page")
         logout_link_text = my_account_signed_in.get_logout_link_text()
